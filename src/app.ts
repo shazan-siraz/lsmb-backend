8d6a497c440de0f609ddad40aca44b27492e4400
siraz015 <siraj015213@gmail.com>
2024-07-30 07:47:25 +0600

    students route are created

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, Response } from "express";
 import cors from "cors";
+import { StudentRoutes } from "./app/modules/student/student.route";
 const app: Application = express();
 const port = 3000;
 
 // parser
 app.use(express.json())
 app.use(cors())
+
+
+// application route
+app.use('/api/v1/students', StudentRoutes)
 
 app.get("/", (req: Request, res: Response) => {
   res.send("PH University Server is running");
