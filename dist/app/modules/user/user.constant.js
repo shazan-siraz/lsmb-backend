530d7ea725206af506db1004194229446082d773
unknown <siraj015213@gmail.com>
2024-10-11 15:21:49 +0600

    'sometihing'

--- dist/app/modules/user/user.constant.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.USER_ROLE = void 0;
-exports.USER_ROLE = {
-    superAdmin: "superAdmin",
-    admin: "admin",
-    manager: "manager",
-    fieldOfficer: "fieldOfficer",
-};
